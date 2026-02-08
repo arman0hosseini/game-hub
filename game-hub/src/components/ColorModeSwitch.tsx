325a29ee9c5f80b16a41b957bebe344d4ee2ee4e
Arman Hosseini <arman0hosseini@gmail.com>
2026-02-08 21:19:22 +0330

    color switch

---
+++ game-hub/src/components/ColorModeSwitch.tsx
@@ -0,0 +1,22 @@
+import { HStack, Switch } from "@chakra-ui/react";
+import { useColorMode } from "./ui/color-mode";
+
+const ColorModeSwitch = () => {
+  const { colorMode, toggleColorMode } = useColorMode();
+
+  return (
+    <HStack>
+      <Switch.Root
+        checked={colorMode === "dark"}
+        onCheckedChange={toggleColorMode}
+        colorPalette={"green"}
+      >
+        <Switch.HiddenInput />
+        <Switch.Control />
+        <Switch.Label>Dark Mode</Switch.Label>
+      </Switch.Root>
+    </HStack>
+  );
+};
+
+export default ColorModeSwitch;
