70aab05d150666263084c88c5b64cb7b4e0a607c
Arman Hosseini <arman0hosseini@gmail.com>
2026-02-14 00:41:47 +0330

    Refactor GameGrid

--- game-hub/src/components/ui/color-mode.tsx
+++ game-hub/src/components/ui/color-mode.tsx
@@ -13,12 +13,7 @@ export interface ColorModeProviderProps extends ThemeProviderProps {}
 
 export function ColorModeProvider(props: ColorModeProviderProps) {
   return (
-    <ThemeProvider
-      attribute="class"
-      disableTransitionOnChange
-      defaultTheme="dark"
-      {...props}
-    />
+    <ThemeProvider attribute="class" disableTransitionOnChange {...props} />
   );
 }
 
